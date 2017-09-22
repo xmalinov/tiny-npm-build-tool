d373221b451879b7b2f933ed6fbe50f9a863df82
Alexander Malinov <malinovbox@gmail.com>
2017-09-22 19:04:03 +0300

    Basic config

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import adf from 'module1';
+
+console.log(adf)
+
+let temp = 'adfsad';
+let t2 = 2;
+
+const a = 'asdf';
+
+console.log(`${temp} ${t2}`)
+
+function* dfs() {}
---
+++ src/js/module1.js
@@ -0,0 +1,3 @@
+let adf = 'adf';
+
+export default adf;
